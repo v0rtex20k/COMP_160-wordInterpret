e9a20465946c22a19e22201df0d5908ffce3a8b2
Victor <vitek@resnet142-42.medford.tufts.edu>
2019-11-12 23:18:39 -0500

    commented ZERO

--- interpret.py
+++ interpret.py
@@ -29,9 +29,11 @@ def wordInterpret(inputNums):
 		return 1
 	elif int(inputNums[0]) == 0:	# if the ORIGINAL number starts with a 0, return 0 immediately
 		return 0
-	elif int(inputNums[1]) == 0:	# if there zeros inside the number, transform them into 9's to ignore them
-		inputNums = inputNums[0] + '9' + inputNums[2:]
-	elif 10*int(inputNums[0]) + int(inputNums[1]) < 27: 	# if the current first two numbers are between 10-26 (only valid 2-digit mappings), keep "good" numbers
+	elif 10*int(inputNums[0]) + int(inputNums[1]) < 27: 	# keep "good" numbers between 10-26 (only valid 2-digit mappings), 
+		if int(inputNums[1]) == 0:		# if there is a zero inside the number next to a "good" number, transform it into 9 (ignore)
+			inputNums = inputNums[0] + '9' + inputNums[2:]
 		return wordInterpret(inputNums[1:]) + wordInterpret(inputNums[2:])
+	elif int(inputNums[1]) == 0: 	# if there is a zero inside the number NOT next to a "good" number, return 0 immediately
+		return 0
 	return wordInterpret(inputNums[1:]) 	# else, ignore "bad" numbers (3-9, since zeros have been transformed)
 
