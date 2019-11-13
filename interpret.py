b3ae69f1438f5b38da1f7d977d5b744a35c1248c
Victor <vitek@resnet142-42.medford.tufts.edu>
2019-11-12 22:42:22 -0500

    final

--- interpret.py
+++ interpret.py
@@ -9,8 +9,8 @@
 #
 ##########################################################################
 
-memo = {}
 def memoizer(func):
+	memo = {}
 	def decorated_wordInterpret(*args):
 		if args not in memo:
 			memo[args] = func(*args)
@@ -26,17 +26,8 @@ def wordInterpret(inputNums):
 	elif int(inputNums[0]) == 0:
 		return 0
 	elif int(inputNums[1]) == 0:
-		inputNums[1] = '9'
+		inputNums = inputNums[0] + '9' + inputNums[2:]
 	elif 10*int(inputNums[0]) + int(inputNums[1]) < 27:
 		return wordInterpret(inputNums[1:]) + wordInterpret(inputNums[2:])
 	return wordInterpret(inputNums[1:])
-
-def main():
-	#inputNums = "15275217996613369643"
-	inputNums = "15275217996613369643172721682123315785815698496179798976913285366972876142616479915849833987811384156577122962271972971658196761372364219795357639692377138168857139699936929646259446571924941454262699"
-	result = wordInterpret(inputNums)
-	print(result)
-
-if __name__ == '__main__':
-	main()
 
